a1b04d28ee9e7bd1534905428bb8a9d9b7fc50d6
virgesmith <a.p.smith@leeds.ac.uk>
2017-01-27 11:31:41 +0000

    desperation is setting in...

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -3,35 +3,35 @@
 context("humanleague")
 
 test_that("marginal sums are invalid", {
-  expect_error(humanleague::synthPop(list(c(10,10),c(10,11))))
+  expect_error(humanleague:::synthPop(list(c(10,10),c(10,11))))
 })
 
 test_that("dimension invalid", {
-  expect_error(humanleague::synthPop(list(c(10,10))))
+  expect_error(humanleague:::synthPop(list(c(10,10))))
 })
 
 test_that("marginal has -ve value", {
-  expect_error(humanleague::synthPop(list(c(10,-10),c(10,-10))))
+  expect_error(humanleague:::synthPop(list(c(10,-10),c(10,-10))))
 })
 
 m<-c(25,25,25,25,25)
 # simple cases where 1 attempt should
 test_that("simple 2D", {
-  res<-humanleague::synthPop(list(m,m))
+  res<-humanleague:::synthPop(list(m,m))
   expect_equal(rowSums(res$x.hat), m)
   expect_lt(res$attempts, 4)
 })
 
 m = m * 125
 test_that("simple 5D", {
-  res<-humanleague::synthPop(list(m,m,m,m,m))
+  res<-humanleague:::synthPop(list(m,m,m,m,m))
   expect_equal(rowSums(res$x.hat), m)
   expect_lt(res$attempts, 4)
 })
 
 m = m * 125
 test_that("simple 8D", {
-  res<-humanleague::synthPop(list(m,m,m,m,m,m,m,m))
+  res<-humanleague:::synthPop(list(m,m,m,m,m,m,m,m))
   expect_equal(rowSums(res$x.hat), m)
   expect_lt(res$attempts, 4)
 })
@@ -39,7 +39,7 @@ test_that("simple 8D", {
 # need to reduce length of dims to get the 12D case to run in any reasonable time
 m = c(2^15,2^15)
 test_that("simple 12D", {
-  res<-humanleague::synthPop(list(m,m,m,m,m,m,m,m,m,m,m,m))
+  res<-humanleague:::synthPop(list(m,m,m,m,m,m,m,m,m,m,m,m))
   expect_equal(rowSums(res$x.hat), m)
   expect_lt(res$attempts, 4)
 })

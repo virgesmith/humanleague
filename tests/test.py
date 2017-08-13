1a2612fefdf4a2b1431550e80b52b46776c20e0b
virgesmith <a.p.smith@leeds.ac.uk>
2017-08-13 21:34:58 +0100

    start putting tests into test harness

--- tests/test.py
+++ tests/test.py
@@ -3,9 +3,28 @@
 import humanleague
 import numpy as np
 
+# temporary
+import sys
+def check(cond):
+  if not cond:
+    sys.exit(1)
+
+#TODO proper unit testing
+from unittest import TestCase
+
+class Test(TestCase):
+
+  # just to ensure test harness works
+  def test_init(self):
+    self.assertTrue(True)
+
+  def test_false(self):
+    self.assertTrue(False)
+
+
 a = humanleague.sobolSequence(3,5)
-print(a)
-a=0
+check(a.size == 15)
+check(a.shape == (5,3))
 
 print(humanleague.sobolSequence(6,10))
 
@@ -23,4 +42,8 @@ print(humanleague.synthPopR([21,0],[1,2,3,4,5,6],3.1))
 #print(humanleague.synthPopG(np.array([4,2]),np.array([1,2,3]),np.array([[1.0, 0.9, 0.8],[0.5, 0.6, 0.7]])))
 
 print(humanleague.numpytest(np.array([[2,3,4],[6,7,8]])))
-#print(humanleague.numpytest([[2,3,4],[6,7,8]]))
+#print(humanleague.numpytest([[2,3,4],[6,7,8]]))    self.assertTrue(True)
+
+
+
+

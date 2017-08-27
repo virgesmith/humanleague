33dc817e5cf2244e7ecca82b44998cb188f311cc
virgesmith <a.p.smith@leeds.ac.uk>
2017-08-27 17:33:00 +0100

    update

--- src/IPF.cpp
+++ src/IPF.cpp
@@ -16,19 +16,6 @@ IPF::IPF(const NDArray<2, double>& seed, const std::array<std::vector<double>, 2
 
 }
 
-int main()
-{
-  std::array<std::vector<double>, 2> m = {std::vector<double>{1,2,3}, 
-                                          std::vector<double>{3,3}};
-
-  size_t size[2] = { m[0].size(), m[1].size() };                                        
-
-  NDArray<2, double> s(size);
-  s.assign(1.0);
-
-  IPF(s, m);
-
-}
   
 // private:
 //   NDArray<2, double> m_result;

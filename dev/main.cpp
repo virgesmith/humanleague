c26e49cfdc6c60a9032dd432709b82d6a6c3f579
virgesmith <a.p.smith@leeds.ac.uk>
2017-09-01 17:31:21 +0100

    generailised IPF to N dimensions

--- dev/main.cpp
+++ dev/main.cpp
@@ -24,6 +24,7 @@ void do2d()
   print(e[0]);
   print(e[1]);
   std::cout << ipf.conv() << ":" << ipf.iters() << std::endl;
+  print(ipf.result().rawData(), ipf.result().storageSize(), m[1].size());
 }
 
 void do3d()
@@ -36,8 +37,8 @@ void do3d()
 
   NDArray<3, double> s(size);
   s.assign(1.0);
-  //Index<3,Index_Unfixed> index(s.sizes());
-  //s[index] = 0.5;
+  // Index<3,Index_Unfixed> index(s.sizes());
+  // s[index] = 0.7;
 
   IPF<3> ipf(s, m);
 
@@ -46,10 +47,37 @@ void do3d()
   print(e[1]);
   print(e[2]);
   std::cout << ipf.conv() << ":" << ipf.iters() << std::endl;
+  print(ipf.result().rawData(), ipf.result().storageSize(), m[1].size());
+}
+
+void do4d()
+{
+  std::vector<std::vector<double>> m = {std::vector<double>{52, 48}, 
+                                        std::vector<double>{87, 13},
+                                        std::vector<double>{67, 33},
+                                        std::vector<double>{55, 45}};
+
+  size_t size[4] = { m[0].size(), m[1].size(), m[2].size(), m[3].size() };                                        
+
+  NDArray<4, double> s(size);
+  s.assign(1.0);
+  //Index<3,Index_Unfixed> index(s.sizes());
+  //s[index] = 0.7;
+
+  IPF<4> ipf(s, m);
+
+  auto e = ipf.errors();
+  print(e[0]);
+  print(e[1]);
+  print(e[2]);
+  print(e[3]);
+  std::cout << ipf.conv() << ":" << ipf.iters() << std::endl;
+  print(ipf.result().rawData(), ipf.result().storageSize(), m[1].size());
 }
 
 int main()
 {
   do2d();
   do3d();
+  do4d();
 }
--- src/IPF.h
+++ src/IPF.h
@@ -17,8 +17,28 @@ public:
 
   static const size_t Dim = D;
 
-  IPF(const NDArray<D, double>& seed, const std::vector<std::vector<double>>& marginals);
-  
+  IPF(const NDArray<D, double>& seed, const std::vector<std::vector<double>>& marginals)
+    : m_result(seed.sizes()), m_marginals(marginals), m_errors(D), m_conv(false)
+  {
+    std::copy(seed.rawData(), seed.rawData() + seed.storageSize(), m_result.begin());
+    for (size_t d = 0; d < Dim; ++d)
+    {
+      m_errors[d].resize(m_marginals[d].size());
+      print(m_marginals[d]);
+    }
+    
+    for (m_iters = 0; !m_conv && m_iters < s_MAXITER; ++m_iters) 
+    {
+      rScale<Dim>(m_result, m_marginals);
+      // inefficient copying?
+      std::vector<std::vector<double>> diffs(Dim);
+
+      rDiff<Dim>(diffs, m_result, m_marginals);
+
+      m_conv = computeErrors(diffs);
+    }
+  }
+    
   IPF(const IPF&) = delete;
   IPF(IPF&&) = delete;
   
@@ -26,6 +46,11 @@ public:
   IPF& operator=(IPF&&) = delete;
   
   virtual ~IPF() { }
+
+  const NDArray<Dim, double>& result() const
+  {
+    return m_result;
+  }
 
   const std::vector<std::vector<double>> errors() const 
   {
@@ -45,31 +70,32 @@ public:
 private:
 
   template<size_t I>
-  void scale()
+  static void rScale(NDArray<D, double>& result, const std::vector<std::vector<double>>& marginals)
   {
-    const size_t Direction = I;
-    const std::vector<double>& r = reduce<Dim, double, Direction>(m_result);
-    for (size_t p = 0; p < m_marginals[Direction].size(); ++p)
+    const size_t Direction = I - 1;
+    const std::vector<double>& r = reduce<D, double, Direction>(result);
+    for (size_t p = 0; p < marginals[Direction].size(); ++p)
     {
-      for (Index<Dim, Direction> index(m_result.sizes(), p); !index.end(); ++index) 
+      for (Index<Dim, Direction> index(result.sizes(), p); !index.end(); ++index) 
       {
-        m_result[index] *= m_marginals[Direction][index[Direction]] / r[p]; 
+        result[index] *= marginals[Direction][index[Direction]] / r[p]; 
       }
     }
+    rScale<I-1>(result, marginals);
   }
 
-  // TODO might be more efficient to directly calc errors (not sure we really need the sign of the values)
-  template<size_t O>
-  void calcResiduals(std::vector<std::vector<double>>& r)
+  template<size_t I>
+  static void rDiff(std::vector<std::vector<double>>& diffs, const NDArray<D, double>& result, const std::vector<std::vector<double>>& marginals)
   {
-    calcResiduals<O-1>(r);
-    r[O-1] = diff(reduce<Dim, double, O-1>(m_result), m_marginals[O-1]);
+    const size_t Direction = I - 1;
+    diffs[Direction] = diff(reduce<Dim, double, Direction>(result), marginals[Direction]);
+    rDiff<I-1>(diffs, result, marginals);
   }
 
   // this is close to repeating the above 
   bool computeErrors(std::vector<std::vector<double>>& diffs)
   {
-    calcResiduals<Dim>(diffs);
+    //calcResiduals<Dim>(diffs);
     double maxError = -std::numeric_limits<double>::max();
     for (size_t d = 0; d < Dim; ++d)
     {
@@ -91,92 +117,49 @@ private:
   size_t m_iters;
   bool m_conv;
   const double m_tol = 1e-8;
-  
 };
 
-// TODO generalise
-template<>
-IPF<2>::IPF(const NDArray<2, double>& seed, const std::vector<std::vector<double>>& marginals)
-  : m_result(seed.sizes()), m_marginals(marginals), m_errors(2), m_conv(false)
-{
-  m_errors[0].resize(m_marginals[0].size());
-  m_errors[1].resize(m_marginals[1].size());
-  print(m_marginals[0]);
-  print(m_marginals[1]);
 
-  std::copy(seed.rawData(), seed.rawData() + seed.storageSize(), m_result.begin());
-  print(reduce<Dim, double, 0>(m_result));
-  print(reduce<Dim, double, 1>(m_result));
+// Specialisation to terminate the recursion for each instantiation 
+# define SPECIALISE_RSCALE(d) \
+template<> \
+template<> \
+void IPF<d>::rScale<0>(NDArray<d, double>&, const std::vector<std::vector<double>>&) { }
 
-  for (m_iters = 0; !m_conv && m_iters < s_MAXITER; ++m_iters) 
-  {
-    scale<0>();
-    //print(m_result.rawData(), m_result.storageSize(), m_marginals[1].size());      
-    scale<1>();
-    // inefficient copying
-    std::vector<std::vector<double>> diffs(Dim);
-    // TODO template loop
-    diffs[0] = reduce<Dim, double, 0>(m_result);
-    diffs[1] = reduce<Dim, double, 1>(m_result);
-    
-    m_conv = computeErrors(diffs);
+SPECIALISE_RSCALE(2)
+SPECIALISE_RSCALE(3)
+SPECIALISE_RSCALE(4)
+SPECIALISE_RSCALE(5)
+SPECIALISE_RSCALE(6)
+SPECIALISE_RSCALE(7)
+SPECIALISE_RSCALE(8)
+SPECIALISE_RSCALE(9)
+SPECIALISE_RSCALE(10)
+SPECIALISE_RSCALE(11)
+SPECIALISE_RSCALE(12)
 
-    print(m_result.rawData(), m_result.storageSize(), m_marginals[1].size());
-  }
-}
+// Specialisation to terminate the diff for each instantiation 
+# define SPECIALISE_RDIFF(d) \
+template<> \
+template<> \
+void IPF<d>::rDiff<0>(std::vector<std::vector<double>>&, const NDArray<d, double>&, const std::vector<std::vector<double>>&) { }
 
-template<>
-IPF<3>::IPF(const NDArray<3, double>& seed, const std::vector<std::vector<double>>& marginals)
-  : m_result(seed.sizes()), m_marginals(marginals), m_errors(3), m_conv(false)
-{
-  m_errors[0].resize(m_marginals[0].size());
-  m_errors[1].resize(m_marginals[1].size());
-  m_errors[2].resize(m_marginals[1].size());
-  print(m_marginals[0]);
-  print(m_marginals[1]);
-  print(m_marginals[2]);
-  
-  std::copy(seed.rawData(), seed.rawData() + seed.storageSize(), m_result.begin());
-  print(reduce<Dim, double, 0>(m_result));
-  print(reduce<Dim, double, 1>(m_result));
-  print(reduce<Dim, double, 2>(m_result));
-  
-  for (m_iters = 0; !m_conv && m_iters < s_MAXITER; ++m_iters) 
-  {
-    scale<0>();
-    scale<0>();
-    //print(m_result.rawData(), m_result.storageSize(), m_marginals[1].size());      
-    scale<1>();
-    scale<2>();
-    // inefficient copying
-    std::vector<std::vector<double>> diffs(Dim);
-    // TODO template loop
-    diffs[0] = reduce<Dim, double, 0>(m_result);
-    diffs[1] = reduce<Dim, double, 1>(m_result);
-    diffs[2] = reduce<Dim, double, 1>(m_result);
-    
-    m_conv = computeErrors(diffs);
+SPECIALISE_RDIFF(2)
+SPECIALISE_RDIFF(3)
+SPECIALISE_RDIFF(4)
+SPECIALISE_RDIFF(5)
+SPECIALISE_RDIFF(6)
+SPECIALISE_RDIFF(7)
+SPECIALISE_RDIFF(8)
+SPECIALISE_RDIFF(9)
+SPECIALISE_RDIFF(10)
+SPECIALISE_RDIFF(11)
+SPECIALISE_RDIFF(12)
 
-    print(m_result.rawData(), m_result.storageSize(), m_marginals[1].size());
-  }
-}
-
-
-// disable 
+// disable trivial and nonsensical dimensionalities
 template<> class IPF<0>;
 template<> class IPF<1>;
-template<> class IPF<4>; //...
 
-// TODO helper macro for member template specialisations
-#define SPECIALISE_CALCRESIDUALS(d) \
-template<> \
-template<> \
-inline void IPF<d>::calcResiduals<1>(std::vector<std::vector<double>>& r) \
-{ \
-  r[0] = diff(reduce<d, double, 0>(m_result), m_marginals[0]); \
-}
-
-SPECIALISE_CALCRESIDUALS(2)
-SPECIALISE_CALCRESIDUALS(3)
-
-#undef SPECIALISE_CALCRESIDUALS
+// remove macro pollution
+#undef SPECIALISE_RSCALE
+#undef SPECIALISE_RDIFF

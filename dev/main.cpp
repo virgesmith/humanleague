db2ed797ca0a459a53709e556990aa3d1c9186c1
virgesmith <a.p.smith@leeds.ac.uk>
2017-09-14 17:03:56 +0100

    #14 progress - generalised index picking, now working in 2/3/4D

--- dev/main.cpp
+++ dev/main.cpp
@@ -7,24 +7,6 @@
 
 #include <iostream>
 
-// delete this impl
-size_t pick(const std::vector<double>& dist, double r)
-{
-  // sum of dist should be 1, but we relax this
-  // r is in (0,1) so scale up r by sum of dist
-  r *= std::accumulate(dist.begin(), dist.end(), 0.0);
-  double runningSum = 0.0;
-  for (size_t i = 0; i < dist.size(); ++i)
-  {
-    runningSum += dist[i];
-    if (r < runningSum)
-      return i;
-  }
-  throw std::runtime_error("pick failed");
-
-}
-
-
 void do2d()
 {
   std::vector<std::vector<double>> m = {std::vector<double>{52, 48}, 
@@ -37,85 +19,20 @@ void do2d()
   //Index<2,Index_Unfixed> index(s.sizes());
   //s[index] = 0.5;
 
-  IPF<2> ipf(s, m);
+  QSIPF<2> qsipf(s, m);
 
-  auto e = ipf.errors();
+  auto e = qsipf.errors();
   print(e[0]);
   print(e[1]);
-  std::cout << ipf.conv() << ":" << ipf.iters() << std::endl;
-  print(ipf.result().rawData(), ipf.result().storageSize(), m[1].size());
+  std::cout << qsipf.conv() << ":" << qsipf.iters() << std::endl;
+  print(qsipf.result().rawData(), qsipf.result().storageSize(), m[1].size());
+  print(qsipf.sample().rawData(), qsipf.sample().storageSize(), qsipf.sample().sizes()[1]);
+  print(reduce<2, uint32_t, 0>(qsipf.sample()));
+  print(reduce<2, uint32_t, 1>(qsipf.sample()));
 }
+
 
 void do3d()
-{
-  // std::vector<std::vector<double>> m = {std::vector<double>{52, 40, 4, 4}, 
-  //                                       std::vector<double>{87, 10, 3},
-  //                                       std::vector<double>{55, 15, 6,12, 12}};
-  std::vector<std::vector<double>> m = {std::vector<double>{32, 32, 32, 32}, 
-                                        std::vector<double>{126, 1, 1},
-                                        std::vector<double>{64, 64}};
-
-  size_t size[3] = { m[0].size(), m[1].size(), m[2].size() };                                        
-
-  NDArray<3, double> s(size);
-  s.assign(1.0);
-
-  IPF<3> ipf(s, m);
-
-  auto e = ipf.errors();
-  print(e[0]);
-  print(e[1]);
-  print(e[2]);
-  std::cout << ipf.conv() << ":" << ipf.iters() << std::endl;
-  print(ipf.result().rawData(), ipf.result().storageSize(), m[1].size());
-
-  // Sample without replacement of static IPF 
-  size_t n = ipf.population();
-  NDArray<3, double>& pop = const_cast<NDArray<3, double>&>(ipf.result());
-  NDArray<3, uint32_t> sample(pop.sizes());
-  sample.assign(0);
-  Sobol qrng(3);
-  const double scale = 0.5 / (1u<<31); 
-  for (size_t i = 0; i < n; ++i)
-  {
-    const std::vector<uint32_t>& r = qrng.buf();
-    size_t index[3] = {0};
-    //for (size_t i = 0; i < n; ++i)
-
-    // reduce dim 0
-    const std::vector<double>& r0 = reduce<3, double, 0>(pop);
-    // pick an index
-    index[0] = pick(r0, r[0] * scale);
-
-    // take slice Dim 0/index 
-    NDArray<2, double> slice0 = slice<3, double, 0>(pop, index[0]);
-    // reduce dim 1 (now 0)
-    const std::vector<double>& r1 = reduce<2, double, 0>(slice0);
-    // pick an index
-    index[1] = pick(r1, r[1] * scale);
-
-    // slice dim 2 (now 0)
-    const std::vector<double>& r2 = slice<double, 0>(slice0, index[1]);
-    // no reduction required
-    // pick an index
-    index[2] = pick(r2, r[2] * scale);
-
-    // without replacement
-    pop[index] = std::max(pop[index] - 1.0, 0.0);
-
-    ++sample[index];
-    //print(index, 3);
-  }
-
-  //print(pop.rawData(), pop.storageSize(), pop.sizes()[1]);
-  print(sample.rawData(), sample.storageSize(), sample.sizes()[1]);
-  print(reduce<3, uint32_t, 0>(sample));
-  print(reduce<3, uint32_t, 1>(sample));
-  print(reduce<3, uint32_t, 2>(sample));
- 
-}
-
-void do3d_dynamic()
 {
   std::vector<std::vector<double>> m = {std::vector<double>{52, 40, 4, 4}, 
                                         std::vector<double>{87, 10, 3},
@@ -129,7 +46,7 @@ void do3d_dynamic()
   NDArray<3, double> s(size);
   s.assign(1.0);
   Index<3, Index_Unfixed> i(s.sizes());
-  s[i] = 0.7;
+  s[i] = 1.0;
 
   QSIPF<3> qsipf(s, m);
 
@@ -162,21 +79,35 @@ void do4d()
   //Index<3,Index_Unfixed> index(s.sizes());
   //s[index] = 0.7;
 
-  IPF<4> ipf(s, m);
+  QSIPF<4> qsipf(s, m);
 
-  auto e = ipf.errors();
+  auto e = qsipf.errors();
   print(e[0]);
   print(e[1]);
   print(e[2]);
   print(e[3]);
-  std::cout << ipf.conv() << ":" << ipf.iters() << std::endl;
-  print(ipf.result().rawData(), ipf.result().storageSize(), m[1].size());
+  //std::cout << qsipf.conv() << ":" << qsipf.iters() << std::endl;
+  print(qsipf.sample().rawData(), qsipf.sample().storageSize(), m[1].size());
+  print(reduce<4, uint32_t, 0>(qsipf.sample()));
+  print(reduce<4, uint32_t, 1>(qsipf.sample()));
+  print(reduce<4, uint32_t, 2>(qsipf.sample()));
+  print(reduce<4, uint32_t, 3>(qsipf.sample()));
 }
 
 int main()
 {
-  //do2d();
-  //do3d();
-  do3d_dynamic();
-  //do4d();
+  try
+  {
+    do2d();
+    do3d();
+    do4d();
+  }
+  catch(const std::exception& e)
+  {
+    std::cout << e.what() << std::endl;
+  }
+  catch(...)
+  {
+    std::cout << "unknown exception" << std::endl;
+  }  
 }
--- src/QSIPF.h
+++ src/QSIPF.h
@@ -5,6 +5,79 @@
 #include "IPF.h"
 #include "Sobol.h"
 
+// TODO move somewhere appropriate (doesnt need to be member)
+inline size_t pick(const std::vector<double>& dist, double r)
+{
+  // sum of dist should be 1, but we relax this
+  // r is in (0,1) so scale up r by sum of dist
+  r *= std::accumulate(dist.begin(), dist.end(), 0.0);
+  double runningSum = 0.0;
+  for (size_t i = 0; i < dist.size(); ++i)
+  {
+    runningSum += dist[i];
+    if (r < runningSum)
+      return i;
+  }
+  throw std::runtime_error("pick failed");
+
+}
+
+
+template<size_t D>
+void decrementMarginals(std::vector<std::vector<double>>& m, const size_t* index)
+{
+  --m[D-1][index[D-1]];
+  decrementMarginals<D-1>(m, index);
+// --(this->m_marginals[1][index[1]]);
+// --(this->m_marginals[2][index[2]]);
+}
+
+// end the recursion
+template<>
+void decrementMarginals<1>(std::vector<std::vector<double>>& m, const size_t* index)
+{
+  --m[0][index[0]];
+}
+
+
+template<size_t D>
+void getIndex(const NDArray<D, double>& p, const std::vector<uint32_t>& r, size_t* index)
+{
+  // TODO template bloat
+  static const double scale = 0.5 / (1u<<31); 
+  
+  // reduce dim D-1
+  const std::vector<double>& m = reduce<D, double, D-1>(p);
+  // pick an index
+  index[D-1] = pick(m, r[D-1] * scale);
+
+  // take slice of Dim D-1 at index 
+  NDArray<D-1, double> sliced = slice<D, double, D-1>(p, index[D-1]);
+
+  // recurse 
+  getIndex<D-1>(sliced, r, index);
+}
+
+// end recursion
+template<>
+void getIndex<2>(const NDArray<2, double>& p, const std::vector<uint32_t>& r, size_t* index)
+{
+  // TODO template bloat
+  static const double scale = 0.5 / (1u<<31); 
+
+  // reduce dim 1 (now 0)
+  const std::vector<double>& r1 = reduce<2, double, 1>(p);
+  // pick an index
+  index[1] = pick(r1, r[1] * scale);
+
+  // slice dim 2 (now 0)
+  const std::vector<double>& r0 = slice<double, 1>(p, index[1]);
+  // no reduction required
+  // pick an index
+  index[0] = pick(r0, r[0] * scale);
+}  
+
+
 template<size_t D>
 class QSIPF : public IPF<D>
 {
@@ -30,37 +103,42 @@ public:
     const size_t n = this->m_population;
     m_sample.assign(0);
     Sobol qrng(D);
-    const double scale = 0.5 / (1u<<31); 
+    //const double scale = 0.5 / (1u<<31); 
+    size_t index[D] = {0};
     for (size_t i = 0; i < n; ++i)
     {
       this->solve(seed);
       if (!this->m_conv)
         throw std::runtime_error("IPF convergence failure");
-      const std::vector<uint32_t>& r = qrng.buf();
-      size_t index[D] = {0};
+      //const std::vector<uint32_t>& r = qrng.buf();
   
-      // reduce dim 0
-      const std::vector<double>& r0 = reduce<D, double, 0>(this->m_result);
-      // pick an index
-      index[0] = pick(r0, r[0] * scale);
+      getIndex<D>(this->m_result, qrng.buf(), &index[0]);
+
+      // // reduce dim 0
+      // const std::vector<double>& r0 = reduce<D, double, 0>(this->m_result);
+      // // pick an index
+      // index[0] = pick(r0, r[0] * scale);
   
-      // take slice of Dim 0 at index 
-      NDArray<D-1, double> slice0 = slice<D, double, 0>(this->m_result, index[0]);
+      // // take slice of Dim 0 at index 
+      // NDArray<D-1, double> slice0 = slice<D, double, 0>(this->m_result, index[0]);
       // reduce dim 1 (now 0)
-      const std::vector<double>& r1 = reduce<D-1, double, 0>(slice0);
-      // pick an index
-      index[1] = pick(r1, r[1] * scale);
+      // const std::vector<double>& r1 = reduce<2, double, 1>(/*sliced*/this->m_result);
+      // // pick an index
+      // index[1] = pick(r1, r[1] * scale);
   
-      // slice dim 2 (now 0)
-      const std::vector<double>& r2 = slice<double, 0>(slice0, index[1]);
-      // no reduction required
-      // pick an index
-      index[2] = pick(r2, r[2] * scale);
-  
+      // // slice dim 0
+      // const std::vector<double>& r0 = slice<double, 1>(this->m_result, index[1]);
+      // // no reduction required
+      // // pick an index
+      // index[0] = pick(r0, r[0] * scale);
+
+      for (size_t i = 0; i < D; ++i)
+        if (index[i] >= this->m_result.sizes()[i])
+          throw std::runtime_error("index out of bounds at " + std::to_string(i) + ": " + std::to_string(index[i]));
+
       // without replacement
-      --(this->m_marginals[0][index[0]]);
-      --(this->m_marginals[1][index[1]]);
-      --(this->m_marginals[2][index[2]]);
+      // directly decrementing the IPF population doesnt converge
+      decrementMarginals<D>(this->m_marginals, index);
       
       ++m_sample[index];
       //print(index, 3);
@@ -73,22 +151,6 @@ public:
   }
 
 private:
-  // TODO move somewhere appropriate (doesnt need to be member)
-  size_t pick(const std::vector<double>& dist, double r)
-  {
-    // sum of dist should be 1, but we relax this
-    // r is in (0,1) so scale up r by sum of dist
-    r *= std::accumulate(dist.begin(), dist.end(), 0.0);
-    double runningSum = 0.0;
-    for (size_t i = 0; i < dist.size(); ++i)
-    {
-      runningSum += dist[i];
-      if (r < runningSum)
-        return i;
-    }
-    throw std::runtime_error("pick failed");
-
-  }
 
   NDArray<D, uint32_t> m_sample;
 

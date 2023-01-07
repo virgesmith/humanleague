4e17ded0020a40fd6e8f8f7138a13e079919c024
virgesmith <andrew@friarswood.net>
2023-01-07 10:07:33 +0000

    ...

--- src/Log.h
+++ src/Log.h
@@ -23,7 +23,7 @@ std::string to_string_impl(T v)
 template<typename T>
 std::string to_string_impl(T* p)
 {
-  constexpr const size_t BUF_SIZE = 20;
+  constexpr size_t BUF_SIZE = 20;
   static char buf[BUF_SIZE];
   std::snprintf(buf, BUF_SIZE, "0x%016zx", reinterpret_cast<size_t>(p));
   return std::string(buf);

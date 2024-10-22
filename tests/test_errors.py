81a00136ce151d175eff0ac18029ab5c5f892c6b
virgesmith <andrew@friarswood.net>
2024-10-22 19:21:28 +0100

    format

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -5,9 +5,8 @@ import humanleague as hl
 
 
 def test_input_validation() -> None:
-
     m1 = np.array([1, 2, 3])
-    m2 = np.ones((2,3))
+    m2 = np.ones((2, 3))
 
     with pytest.raises(RuntimeError):
         # index list size 1 too small or differs from marginal size 1
@@ -15,11 +14,11 @@ def test_input_validation() -> None:
 
     with pytest.raises(RuntimeError):
         # problem needs to have more than 1 dimension!
-        hl.ipf(np.ones(3),[(0,), (0,)], [m1, m1])
+        hl.ipf(np.ones(3), [(0,), (0,)], [m1, m1])
 
     with pytest.raises(RuntimeError):
         # index/marginal dimension mismatch 2 vs 1
-        hl.ipf(np.ones(3),[(0,), (1, 2)], [m1, m1])
+        hl.ipf(np.ones(3), [(0,), (1, 2)], [m1, m1])
 
     with pytest.raises(RuntimeError):
         #  mismatch at index 1: dimension 1 size 3 redefined to 2

e28f541b8c2a1d3cafa1298802a4765e8ec18112
Andrew Smith <virgesmith@users.noreply.github.com>
2024-10-23 19:39:16 +0100

    CRAN tweaks (#48)
    * CRAN tweaks

--- scripts/package.sh
+++ scripts/package.sh
@@ -7,10 +7,10 @@ echo $version
 
 # package
 # no binary dist as C++
-python3 setup.py sdist
+python -m build . --sdist
 # upload
-# twine upload -u __token__ -p $TEST_PYPI_API_TOKEN  --repository-url https://test.pypi.org/legacy/ dist/humanleague-$version.tar.gz
-twine upload -u __token__ -p $PYPI_API_TOKEN --repository-url https://upload.pypi.org/legacy/ dist/humanleague-$version.tar.gz
+# twine upload -u __token__ -p $TEST_PYPI_API_TOKEN  --repository testpypi dist/humanleague-$version.tar.gz
+twine upload -u __token__ -p $PYPI_API_TOKEN dist/humanleague-$version.tar.gz
 
 # NB on testPyPI, deps need to been installed from main repo. Use this:
 # pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple humanleague
